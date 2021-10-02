170da372872e1e863ee636c9cfcd9d01b7a7942b
S0UPernova <mtsk321@gmail.com>
2021-10-02 00:15:44 -0700

    maybe it was the plugins that made no difference

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -6,11 +6,11 @@ const { webpack } = require('webpack')
 
 // const webpack = require('webpack')
 
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
-  })
-)
+// environment.plugins.prepend('Provide',
+//   new webpack.ProvidePlugin({
+//     $: 'jquery/src/jquery',
+//     jQuery: 'jquery/src/jquery'
+//   })
+// )
 
 module.exports = environment
