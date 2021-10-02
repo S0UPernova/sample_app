23af8a387c624f7cb0077a5a5bb2389cc847c3c4
S0UPernova <mtsk321@gmail.com>
2021-10-01 23:23:31 -0700

    Iplemented basic login

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   # get 'static_pages/home'
-  get '/help', to: 'static_pages#help'
-  get '/about', to:'static_pages#about'
-  get '/hello', to:'application#hello'
-  get '/contact', to:'static_pages#contact'
-  get '/signup', to: 'users#new'
+  get '/help',      to: 'static_pages#help'
+  get '/about',     to:'static_pages#about'
+  get '/hello',     to:'application#hello'
+  get '/contact',   to:'static_pages#contact'
+  get '/signup',    to: 'users#new'
   resources :users
+  get '/login',     to: 'sessions#new'
+  post '/login',    to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
 const { environment } = require('@rails/webpacker')
+const { webpack } = require('webpack')
+
+// in the book, but does not seem to be needed
+// although I am leaving it in 
+
+// const webpack = require('webpack')
+
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+)
 
 module.exports = environment
